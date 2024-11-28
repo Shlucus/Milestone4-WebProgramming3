9dfe363309e9189c55c664492fd3ce4fe7e42ff3
Lucas Lalumière <lucas.lalum@gmail.com>
2024-11-28 12:16:43 -0500

    Obtaining a single product in the products microservice

--- ECommerce.Api.Products/Providers/ProductsProvider.cs
+++ ECommerce.Api.Products/Providers/ProductsProvider.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ECommerce.Api.Products.Db;
 using ECommerce.Api.Products.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -37,9 +38,23 @@ namespace ECommerce.Api.Products.Providers
         }
 
 
-        public Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync()
+        public async Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var products = await dbContext.Products.ToListAsync();
+                if (products != null && products.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Product>, IEnumerable<Models.Product>>(products);
+                    return (true, result, null);
+                }
+                return (false, null, "Not Found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
         }
     }
 }
