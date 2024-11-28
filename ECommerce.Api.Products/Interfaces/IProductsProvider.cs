6de46fd8bb9c1b9b585c4926da1914a065533475
Lucas Lalumière <lucas.lalum@gmail.com>
2024-11-28 12:31:04 -0500

    Obtaining all the products in the products microservice

--- ECommerce.Api.Products/Interfaces/IProductsProvider.cs
+++ ECommerce.Api.Products/Interfaces/IProductsProvider.cs
@@ -6,5 +6,6 @@ namespace ECommerce.Api.Products.Interfaces
     public interface IProductsProvider
     {
         Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync();
+        Task<(bool IsSuccess, Models.Product Product, string ErrorMessage)> GetProductAsync(int id);
     }
 }
