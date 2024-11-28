66cd072136d654f68fc3a4a501fc998325bc5ef7
Lucas Lalumière <lucas.lalum@gmail.com>
2024-11-28 11:51:55 -0500

    Creating the database context for the products microservice

--- ECommerce.Api.Products/Db/ProductsDbContext.cs
+++ ECommerce.Api.Products/Db/ProductsDbContext.cs
@@ -4,5 +4,10 @@ namespace ECommerce.Api.Products.Db
 {
     public class ProductsDbContext: DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public ProductsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
