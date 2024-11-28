f2daf291c29fb11b234b908917efaeca51a6de02
Lucas Lalumière <lucas.lalum@gmail.com>
2024-11-28 12:05:38 -0500

    Creating the products provider

---
+++ ECommerce.Api.Products/Interfaces/IProductsProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Interfaces
+{
+    public interface IProductsProvider
+    {
+        Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync();
+    }
+}
