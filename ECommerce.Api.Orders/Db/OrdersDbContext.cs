c1b271e24e36e245f7ee5cf647985a5f6320e8d9
Lucas Lalumière <lucas.lalum@gmail.com>
2024-11-28 19:45:45 -0500

    Creating the database context for the orders microservice

--- ECommerce.Api.Orders/Db/OrdersDbContext.cs
+++ ECommerce.Api.Orders/Db/OrdersDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ECommerce.Api.Orders.Db
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Orders.Db
 {
-    public class OrdersDbContext
+    public class OrdersDbContext : DbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public OrdersDbContext(DbContextOptions<OrdersDbContext> options) : base(options)
+        {
+        }
     }
 }
--- ECommerce.Api.Orders/Startup.cs
+++ ECommerce.Api.Orders/Startup.cs
@@ -1,6 +1,8 @@
+using ECommerce.Api.Orders.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,11 @@ namespace ECommerce.Api.Orders
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<OrdersDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Orders");
+
+            });
             services.AddControllers();
         }
 
